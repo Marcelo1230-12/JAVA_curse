5f28348816cb9d521abd17c85f97a40606f017d7
Angel Minchala <149975350+Marcelo1230-12@users.noreply.github.com>
2026-07-09 15:19:12 -0400

    Problems solve

---
+++ src/main/java/Expretion_evaluation.java
@@ -0,0 +1,7 @@
+public class Expretion_evaluation {
+    public static void main(String[] args) {
+
+        double total = ((25.5*3.5-3.5*3.5)/(40.5-4.5));
+        System.out.println("Total is "+ total);
+    }
+}
