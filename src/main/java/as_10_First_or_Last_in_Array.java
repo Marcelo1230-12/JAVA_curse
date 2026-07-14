fdaae0a615e6ac57e9b87f10e0a7c2bd58a9a1cb
Angel Minchala <149975350+Marcelo1230-12@users.noreply.github.com>
2026-07-14 18:27:40 -0400

    WORK 7/14/2026

--- src/main/java/as_10_First_or_Last_in_Array.java
+++ src/main/java/as_10_First_or_Last_in_Array.java
@@ -1,11 +1,34 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class as_10_First_or_Last_in_Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int n ;
+        System.out.println("Enter the array size");
+    n = sc.nextInt();
+    int[] a = new int[n];
+    for (int i=0;i<n;i++){
+        System.out.println("Enter the element "+"#"+i);
+        a[i]=sc.nextInt();
+    }
 
+    for (int i=0;i<n;i++){
+        if (a[0]==10 && a[n-1]==10){
+            System.out.println("true");
+            break;
+        }
+        else{
+            System.out.println("false");
+            break;
+        }
+    }
+        System.out.println("The array is:");
+for (int i:a){
+    System.out.print(i+" ");
+}
         sc.close();
-
     }
 }
+
