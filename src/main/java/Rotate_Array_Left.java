60765dc49132210a8c9093e51ea57ca328cca2aa
Angel Minchala <149975350+Marcelo1230-12@users.noreply.github.com>
2026-07-16 15:32:45 -0400

    WORK 7/16/2026

---
+++ src/main/java/Rotate_Array_Left.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Rotate_Array_Left {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+int lng;
+        System.out.print("Enter the size of the array");
+        lng = sc.nextInt();
+        int  []list = new int[lng];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<lng;i++){
+            list[i]=sc.nextInt();
+        }
+        int  []list2 = new int[lng];
+        for (int i = 0; i < lng - 1; i++) {
+            list2[i] = list[i + 1];
+        }
+        list2[lng - 1] = list[0];
+        for(int a : list2){
+            System.out.print("["+a+","+"]");
+        }
+
+        sc.close();
+    }
+}
