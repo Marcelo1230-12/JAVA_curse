eb2bfcbef76067526bdefc5a16bb7ee2ccfe7756
Angel Minchala <149975350+Marcelo1230-12@users.noreply.github.com>
2026-07-17 16:15:07 -0400

    WORK 7/17/2026

--- src/main/java/First_and_Last_Elements_Swap.java
+++ src/main/java/First_and_Last_Elements_Swap.java
@@ -12,10 +12,7 @@ public class First_and_Last_Elements_Swap {
             list[i]=sc.nextInt();
         }
         int []list2 = new int[numbe];
-//        for (int i = 0;i<numbe-1;i++){
-//            list2[i]=list[i+1];//10 20 30
-//                               //20 30 10
-//        }
+
         list2[0]=list[numbe-1];
         list2[numbe-1]=list[0];
         for(int i=1;i<numbe-1;i++){
