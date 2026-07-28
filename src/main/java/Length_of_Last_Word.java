c32fbcd56a2caea1accda93e0cc75eb2677ee7ed
Marcelo1230-12 <marcelomainchala@gmail.com>
2026-07-27 20:28:01 -0400

    Work 07/27/2026

---
+++ src/main/java/Length_of_Last_Word.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Length_of_Last_Word {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str;
+        System.out.println("Enter a string");
+        str = sc.nextLine();
+       int count = 0;
+
+     for(int i = str.length()-1; i>0;i--){
+if(str.charAt(i)==' '){
+    break;
+}
+else {
+    count++;
+}
+     }
+        System.out.println("The last word is: "+count);
+        sc.close();
+    }
+}
